ee6ca95940ec29fe8f676a01a9e2ff04ea11f910
marios1861 <marios1861@gmail.com>
2022-12-31 19:52:54 +0200

    Add coder/decoder and produce decoded soundfile
    - Not working

--- sim.py
+++ sim.py
@@ -1,44 +1,101 @@
+import argparse
 from pathlib import Path
+from typing import List, Tuple
 import numpy as np
 from mp3 import make_mp3_analysisfb, make_mp3_synthesisfb
 import matplotlib.pyplot as plt
+from scipy.io import wavfile
+from scipy.signal import convolve
+from math import floor
+from nothing import donothing, idonothing
 
 
-def main():
-    M = 32
-    h = np.load(Path("h.npy"), allow_pickle=True).item()['h'].flatten()
+def codec0(wavin: Path, h: np.ndarray, M: int, N: int) -> Tuple[np.ndarray, np.ndarray]:
+    Ytot = coder0(wavin, h, M, N)
+    Ytot_list = [Ytot[i: (i + N), :] for i in range(0, floor(Ytot.shape[0] / N))]
+    xhat = decoder0(Ytot_list, h, M, N)
+    return (Ytot, xhat)
+
+
+def coder0(wavin: Path, h: np.ndarray, M: int, N: int) -> np.ndarray:
+    sample_rate, data = wavfile.read(wavin)
     H = make_mp3_analysisfb(h, M)
+    Y = [convolve(data, h_i, mode="same") for h_i in H.T]
+    framed_data = [[y[i: (i + N)] for y in Y] for i in range(0, floor(Y[0].size / N))]
+    Ytot = []
+    for frame in framed_data:
+        frame = np.stack(frame, axis=1)
+        Yc = donothing(frame)
+        Ytot.append(Yc)
+    Ytot = np.vstack(Ytot)
+    return Ytot
+
+
+def decoder0(Ytot: List[np.ndarray], h: np.ndarray, M: int, N: int) -> np.ndarray:
     G = make_mp3_synthesisfb(h, M)
-    # plot transfer functions of h_i (in dB)
-    fig1 = plt.figure(num=0)
-    fig2 = plt.figure(num=1)
-    cutoff_db = -30
-    # calculate frequency axis
+    Yh_tot = []
+    for frame in Ytot:
+        Yh = idonothing(frame)
+        Yh_tot.append(Yh)
+    Yh_tot = np.vstack(Ytot)
+    R = [convolve(yH, g_i, mode="same") for (yH, g_i) in zip(Yh_tot.T, G.T)]
+    R = np.vstack(R).T
+    print(R.shape)
+    xhat = np.sum(R, axis=1)
+    return xhat
+
+
+def main(args):
+    M = 32
+    N = 36
     sample_rate = 44100
-    h_freq = np.fft.rfftfreq(H.shape[0], 1. / sample_rate)
-    h_bark = 13 * np.arctan(0.00076 * h_freq) + 3.5 * np.arctan((h_freq / 7500) ** 2) 
-    print(h_bark.shape, h_freq.shape)
-    for i in range(0, M):
-        # calculate FFT
-        # since signal is real, we use rfft
-        h_fft = np.fft.rfft(H[:, i])
-        # cutoff at cutoff_db to remove low magnitude noise from plot
-        h_dB = np.maximum(20 * np.log10(np.absolute(h_fft)), cutoff_db)
+    h = np.load(Path("h.npy"), allow_pickle=True).item()["h"].flatten()
+
+    if args.plot:
+        # plot transfer functions of h_i (in dB)
+        H = make_mp3_analysisfb(h, M)
+        G = make_mp3_synthesisfb(h, M)
+        fig1 = plt.figure(num=0)
+        fig2 = plt.figure(num=1)
+        # calculate frequency axis
+        cutoff_db = -30
+        h_freq = np.fft.rfftfreq(H.shape[0], 1.0 / sample_rate)
+        h_bark = 13 * np.arctan(0.00076 * h_freq) + 3.5 * np.arctan(
+            (h_freq / 7500) ** 2
+        )
+        for i in range(0, M):
+            # calculate FFT
+            # since signal is real, we use rfft
+            h_fft = np.fft.rfft(H[:, i])
+            # cutoff at cutoff_db to remove low magnitude noise from plot
+            h_dB = np.maximum(20 * np.log10(np.absolute(h_fft)), cutoff_db)
+            plt.figure(num=0)
+            plt.plot(h_freq, h_dB)
+            plt.figure(num=1)
+            plt.plot(h_bark, h_dB)
         plt.figure(num=0)
-        plt.plot(h_freq, h_dB)
+        plt.xlabel("Frequency [Hz]")
+        plt.ylabel("Magnitude [dB]")
+        fig1.suptitle("Magnitude of transfer functions over Hz", fontsize=14)
         plt.figure(num=1)
-        plt.plot(h_bark, h_dB)
-    plt.figure(num=0)
-    plt.xlabel("Frequency [Hz]")
-    plt.ylabel("Magnitude [dB]")
-    fig1.suptitle("Magnitude of transfer functions over Hz", fontsize=14)
-    plt.figure(num=1)
-    plt.xlabel("Frequency [bark]")
-    plt.ylabel("Magnitude [dB]")
-    fig2.suptitle("Magnitude of transfer functions over barks", fontsize=14)
-    
-    plt.show()
+        plt.xlabel("Frequency [bark]")
+        plt.ylabel("Magnitude [dB]")
+        fig2.suptitle("Magnitude of transfer functions over barks", fontsize=14)
+
+        plt.show()
+
+    Ytot, xhat = codec0(Path(args.file), h, M, N)
+    wavfile.write(Path(f"modified_{args.file}"), sample_rate, xhat)
+
+
+def parse_opt():
+    parser = argparse.ArgumentParser(description="A wav to mp3 converter")
+    parser.add_argument(
+        "--file", type=str, default="myfile.wav", help="The wav file to be used"
+    )
+    parser.add_argument("--plot", action="store_true", help="if provided, plots")
+    return parser.parse_args()
 
 
 if __name__ == "__main__":
-    main()
+    main(parse_opt())
